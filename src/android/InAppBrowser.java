5c6b04493f165aa2796615a20cc1e49d678e5858
hasib2cool <39165424+hasib2cool@users.noreply.github.com>
2021-05-04 11:43:51 +0400

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -221,6 +221,7 @@ public class InAppBrowser extends CordovaPlugin {
                         {
                             try {
                                 LOG.d(LOG_TAG, "loading in dialer");
+								url = url.replace("uaepass","uaepassqa");
                                 Intent intent = new Intent(Intent.ACTION_DIAL);
                                 intent.setData(Uri.parse(url));
                                 cordova.getActivity().startActivity(intent);
@@ -456,6 +457,7 @@ public class InAppBrowser extends CordovaPlugin {
             intent = new Intent(Intent.ACTION_VIEW);
             // Omitting the MIME type for file: URLs causes "No Activity found to handle Intent".
             // Adding the MIME type to http: URLs causes them to not be handled by the downloader.
+			url = url.replace("uaepass","uaepassqa");
             Uri uri = Uri.parse(url);
             if ("file".equals(uri.getScheme())) {
                 intent.setDataAndType(uri, webView.getResourceApi().getMimeType(uri));
@@ -1194,6 +1196,7 @@ public class InAppBrowser extends CordovaPlugin {
             if (url.startsWith(WebView.SCHEME_TEL)) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_DIAL);
+					url = url.replace("uaepass","uaepassqa");
                     intent.setData(Uri.parse(url));
                     cordova.getActivity().startActivity(intent);
                     override = true;
@@ -1203,6 +1206,7 @@ public class InAppBrowser extends CordovaPlugin {
             } else if (url.startsWith("geo:") || url.startsWith(WebView.SCHEME_MAILTO) || url.startsWith("market:") || url.startsWith("intent:")) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
+					url = url.replace("uaepass","uaepassqa");
                     intent.setData(Uri.parse(url));
                     cordova.getActivity().startActivity(intent);
                     override = true;
@@ -1214,7 +1218,7 @@ public class InAppBrowser extends CordovaPlugin {
             else if (url.startsWith("sms:")) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
-
+                 url  = url.replace("uaepass","uaepassqa");
                     // Get address
                     String address = null;
                     int parmIndex = url.indexOf('?');
